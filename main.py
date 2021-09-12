9669bdf48b479408530845470650a0c7535f4d85
BuildTools <bmahdjoubilel@gmail.com>
2021-09-12 19:20:03 +0200

    v1.2

--- main.py
+++ main.py
@@ -14,43 +14,52 @@ screen.fill([255,255,255])
 screen.blit(barrier , [200,0])
 game_surface = pygame.Surface([640,640])
 game_surface.fill([215,255,215])
-run = True
-grid_size = 50
 clock = pygame.time.Clock()
 snake = Snake()
 interval=0
 
-while run:
+run = True
+run_game = True
 
-     keys = pygame.key.get_pressed()
-
-     if interval % 30 == 0 and interval != 0:
-          snake.move(screen,game_surface)
-          for n in snake.list:
-               if n[1]==snake.list[len(snake.list)-1][1] and n != snake.list[len(snake.list)-1]:
-                    game_surface.fill([215,255,215])
-                    snake = Snake()
-                    snake.move_side = "RIGHT"
-               elif n[1][0] < 0 or n[1][1] < 0 or n[1][0] > gs_surface[0]-32 or n[1][1] > gs_surface[1]-32:
-                    game_surface.fill([215,255,215])
-                    snake = Snake()
-                    snake.move_side = "RIGHT"
-     
-     if (keys[K_RIGHT]-keys[K_LEFT] == 1):
-          snake.move_side =  'RIGHT'
-     elif (keys[K_RIGHT]-keys[K_LEFT] == -1):
-          snake.move_side =  'LEFT'
-     elif (keys[K_UP]-keys[K_DOWN] == 1):
-          snake.move_side =  'UP'
-     elif (keys[K_UP]-keys[K_DOWN] == -1):
-          snake.move_side =  'DOWN'
-
+def events():
+     global run
+     global run_game
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
-               run = False
-     
-     screen.blit(game_surface,[232,32])
-     pygame.display.flip()
-     clock.tick(200)
+                    run = False
+                    run_game = False
 
-     interval += 1
+while run:
+
+     while run_game:
+
+          events()
+
+          keys = pygame.key.get_pressed()
+
+          if interval % 30 == 0 and interval != 0:
+               snake.move(screen,game_surface)
+               for n in snake.list:
+                    if n[1]==snake.list[len(snake.list)-1][1] and n != snake.list[len(snake.list)-1]:
+                         game_surface.fill([215,255,215])
+                         snake = Snake()
+                         snake.move_side = "RIGHT"
+                    elif n[1][0] < 0 or n[1][1] < 0 or n[1][0] > gs_surface[0]-32 or n[1][1] > gs_surface[1]-32:
+                         game_surface.fill([215,255,215])
+                         snake = Snake()
+                         snake.move_side = "RIGHT"
+          
+          if (keys[K_RIGHT]-keys[K_LEFT] == 1):
+               snake.move_side =  'RIGHT'
+          elif (keys[K_RIGHT]-keys[K_LEFT] == -1):
+               snake.move_side =  'LEFT'
+          elif (keys[K_UP]-keys[K_DOWN] == 1):
+               snake.move_side =  'UP'
+          elif (keys[K_UP]-keys[K_DOWN] == -1):
+               snake.move_side =  'DOWN'
+          
+          screen.blit(game_surface,[232,32])
+          pygame.display.flip()
+          clock.tick(200)
+
+          interval += 1
--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import  time
 
 class Snake:
 
@@ -11,13 +12,13 @@ class Snake:
           self.move_side = "RIGHT"
           self.food = None
 
-          surface = pygame.Surface([32,32])
-          surface.fill([255,0,0])
+          surface = pygame.image.load("./img/snake_fragments/frag_8.png")
+          surface.convert_alpha()
           self.list.append([surface,[9*32,9*32]])
 
      
      def move(self,screen ,game_screen):
-          surface = pygame.Surface([32,32])
+          surface = pygame.image.load("./img/snake_fragments/frag_1.png")
           if self.move_side == "RIGHT":
                pos = [self.list[len(self.list)-1][1][0] +32 ,self.list[len(self.list)-1][1][1]]
           elif self.move_side == "LEFT":
@@ -26,7 +27,6 @@ class Snake:
                pos = [self.list[len(self.list)-1][1][0],self.list[len(self.list)-1][1][1] +32]
           elif self.move_side == "UP":
                pos = [self.list[len(self.list)-1][1][0],self.list[len(self.list)-1][1][1] -32]
-          surface.fill([255,255,255])
           self.list.append([surface,pos])
 
           if len(self.list)-1>self.lenght-1:
@@ -34,15 +34,60 @@ class Snake:
                game_screen.blit(self.list[0][0],self.list[0][1])
                self.list.remove(self.list[0])
 
-          for i in self.list:
-               
-               h = 191+(int(64 / self.lenght)*(self.list.index(i)+1))
+          for a,i in enumerate(self.list):
 
-               i[0].fill([h,0,0])
+               if i == self.list[len(self.list)-1]:
+                    i[0].fill([215,255,215])
+                    if(self.move_side == "RIGHT"): surf = pygame.image.load("./img/snake_fragments/frag_8.png")
+                    elif(self.move_side == "LEFT"): surf = pygame.image.load("./img/snake_fragments/frag_10.png")
+                    elif(self.move_side == "DOWN"): surf = pygame.image.load("./img/snake_fragments/frag_9.png")
+                    elif(self.move_side == "UP"): surf = pygame.image.load("./img/snake_fragments/frag_11.png")
+                    i[0].blit(surf,(0,0))
+               elif i == self.list[0]:
+                    if i[1][0] == self.list[1][1][0]-32:
+                         i[0].fill([215,255,215])
+                         i[0].blit(pygame.image.load("./img/snake_fragments/frag_7.png"),[0,0])
+                    elif i[1][0] == self.list[1][1][0]+32:
+                         i[0].fill([215,255,215])
+                         i[0].blit(pygame.transform.rotate(pygame.image.load("./img/snake_fragments/frag_7.png"), 180),[0,0])
+                    elif i[1][1] == self.list[1][1][1]+32:
+                         i[0].fill([215,255,215])
+                         i[0].blit(pygame.transform.rotate(pygame.image.load("./img/snake_fragments/frag_7.png"), 90),[0,0])
+                    elif i[1][1] == self.list[1][1][1]-32:
+                         i[0].fill([215,255,215])
+                         i[0].blit(pygame.transform.rotate(pygame.image.load("./img/snake_fragments/frag_7.png"), -90),[0,0])
+               elif (i[1][0] == self.list[a+1][1][0]-32 and i[1][0] == self.list[a-1][1][0]+32) or (i[1][0] == self.list[a-1][1][0]-32 and i[1][0] == self.list[a+1][1][0]+32):
+                    i[0].fill([215,255,215])
+                    surf = pygame.image.load("./img/snake_fragments/frag_1.png")
+                    i[0].blit(surf,(0,0))
+               elif (i[1][1] == self.list[a+1][1][1]-32 and i[1][1] == self.list[a-1][1][1]+32) or (i[1][1] == self.list[a-1][1][1]-32 and i[1][1] == self.list[a+1][1][1]+32):
+                    i[0].fill([215,255,215])
+                    surf = pygame.transform.rotate(pygame.image.load("./img/snake_fragments/frag_1.png"),90)
+                    i[0].blit(surf,(0,0))
+               elif (i[1][1] == self.list[a+1][1][1]+32 and i[1][0] == self.list[a-1][1][0]-32) or (i[1][1] == self.list[a-1][1][1]+32 and i[1][0] == self.list[a+1][1][0]-32):
+                    i[0].fill([215,255,215])
+                    surf = pygame.image.load("./img/snake_fragments/frag_4.png")
+                    i[0].blit(surf,(0,0))
+               elif (i[1][1] == self.list[a+1][1][1]-32 and i[1][0] == self.list[a-1][1][0]-32) or (i[1][1] == self.list[a-1][1][1]-32 and i[1][0] == self.list[a+1][1][0]-32):
+                    i[0].fill([215,255,215])
+                    surf = pygame.image.load("./img/snake_fragments/frag_5.png")
+                    i[0].blit(surf,(0,0))
+               elif (i[1][1] == self.list[a+1][1][1]+32 and i[1][0] == self.list[a-1][1][0]+32) or (i[1][1] == self.list[a-1][1][1]+32 and i[1][0] == self.list[a+1][1][0]+32):
+                    i[0].fill([215,255,215])
+                    surf = pygame.image.load("./img/snake_fragments/frag_3.png")
+                    i[0].blit(surf,(0,0))
+               elif (i[1][1] == self.list[a+1][1][1]-32 and i[1][0] == self.list[a-1][1][0]+32) or (i[1][1] == self.list[a-1][1][1]-32 and i[1][0] == self.list[a+1][1][0]+32):
+                    i[0].fill([215,255,215])
+                    surf = pygame.image.load("./img/snake_fragments/frag_6.png")
+                    i[0].blit(surf,(0,0))
 
                game_screen.blit(i[0],i[1])
           
           if(not self.is_food_here):
+               if self.food != None:
+                    surface = pygame.Surface((32,32))
+                    surface.fill([215,255,215])
+                    game_screen.blit(surface,self.food[1])
                self.spawnFood(game_screen)
                self.score += 1
                surface = pygame.Surface((200,672+32))
@@ -67,7 +112,9 @@ class Snake:
                for n in self.list:
                     if self.food[1]==n[1]:
                          search = True
-          game_surface.blit(self.food[0],self.food[1])
+               
+          if not self.lenght >= 361:
+               game_surface.blit(self.food[0],self.food[1])
                
           self.is_food_here = True
 
